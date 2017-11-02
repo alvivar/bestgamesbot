3f3fabd8ec78ab6a9516ac1a5401b9ee32b8027d
alvivar <andresalvivar@gmail.com>
2017-11-01 20:00:27 -0600

    Bot that queues the best Itchio games into Tumblr!

---
+++ itchioscrapper.py
@@ -0,0 +1,80 @@
+""" Itch.io scrapper that downloads popular games data. """
+
+import json
+import os
+import sys
+import time
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def chunker(seq, size):
+    """ Zip a list into chunks. """
+    return [seq[pos:pos + size] for pos in range(0, len(seq), size)]
+
+
+def get_games():
+    """ Returns a dictionary with all the games data. """
+
+    url = "https://itch.io/games"
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
+    }
+    page = requests.get(url, headers=headers)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    games = {}
+    for data in soup.find_all("div", "game_cell"):
+
+        url = data.find("a", "thumb_link")['href']
+        title = data.find("div", "game_title").a.text
+        description = data.find("div", "game_text")
+        author = data.find("div", "game_author").a.text
+        author_url = data.find("div", "game_author").a['href']
+        price = data.find("div", "price_value")
+
+        image = data.find("div", "game_thumb")['data-background_image']
+        gif = data.find("div", "gif_overlay")
+
+        windows = data.find("span", "icon-windows8")
+        mac = data.find("span", "icon-apple")
+        linux = data.find("span", "icon-tux")
+        web = data.find("span", "web_flag")
+        android = data.find("span", "icon-android")
+
+        games[url] = {
+            'title': title,
+            'description': description.text if description else False,
+            'author': author,
+            'author_url': author_url,
+            'price': price.text if price else False,
+            'image': image,
+            'gif': gif['data-gif'] if gif else False,
+            'windows': True if windows else False,
+            'mac': True if mac else False,
+            'linux': True if linux else False,
+            'web': True if web else False,
+            'android': True if android else False
+        }
+
+    return games
+
+
+if __name__ == "__main__":
+
+    # frozen / not frozen, cxfreeze compatibility
+    DIR = os.path.normpath(
+        os.path.dirname(
+            sys.executable if getattr(sys, 'frozen', False) else __file__))
+    os.chdir(DIR)
+
+    DELTA = time.time()
+
+    GAMES = get_games()
+
+    with open("itchio-games.json", "w") as f:
+        json.dump(GAMES, f)
+
+    print(f"\nAll done! ({round(time.time()-DELTA)}s)")
---
+++ tumblrqueuer.py
@@ -0,0 +1,95 @@
+""" Script that queue Tumblr posts from the Itch.io scrapper. """
+
+import json
+import os
+import sys
+import time
+
+import itchioscrapper
+import pytumblr
+
+if __name__ == "__main__":
+
+    DELTA = time.time()
+
+    # frozen / not frozen, cxfreeze compatibility
+    DIR = os.path.normpath(
+        os.path.dirname(
+            sys.executable if getattr(sys, 'frozen', False) else __file__))
+    os.chdir(DIR)
+
+    # Files
+    TOKENS_FILE = "tokens.json"
+    DONE_FILE = "done.json"
+
+    # Tumblr tokens are mandatory
+    try:
+        TOKENS = json.load(open(TOKENS_FILE, 'r'))
+    except (IOError, ValueError):
+        TOKENS = {
+            'consumer_key': "",
+            'consumer_secret': "",
+            'oauth_token': "",
+            'oauth_secret': ""
+        }
+        with open(TOKENS_FILE, "w") as f:
+            json.dump(TOKENS, f)
+
+    if not TOKENS['consumer_key']:
+        print(
+            f"Tumblr tokens are needed! Fill out {TOKENS_FILE}\nAnd try again..."
+        )
+        sys.exit(0)
+
+    # Tumblr connection
+    API = pytumblr.TumblrRestClient(
+        TOKENS['consumer_key'],
+        TOKENS['consumer_secret'],
+        TOKENS['oauth_token'],
+        TOKENS['oauth_secret'],
+    )
+
+    # Already queued games
+    try:
+        DONE = json.load(open(DONE_FILE, 'r'))
+    except (IOError, ValueError):
+        DONE = []
+
+    # Queue new games
+    GAMES = itchioscrapper.get_games()
+    GAMES = {k: v for k, v in GAMES.items() if k not in DONE}
+
+    for k, v in GAMES.items():
+
+        price = f"Buy it for {v['price']}" if v['price'] else "Free to play"
+
+        win = "Windows " if v['windows'] else ""
+        mac = "Mac " if v['mac'] else ""
+        lin = "Linux " if v['linux'] else ""
+        web = "Web " if v['web'] else ""
+        android = "Android " if v['android'] else ""
+
+        platforms = (win + mac + lin + web + android).strip()
+        platforms_title = platforms.replace(" ", ", ")
+
+        tags = v['author'].split() + v['title'].split() + platforms.split()
+        tags = [t.strip().lower() for t in tags]
+
+        title = f"[{v['title']}]({k}) ([{v['author']}]({v['author_url']}))"
+
+        API.create_photo(
+            "bestgamesintheplanet",
+            state="queue",
+            tags=["indie", "games", "itchio"] + tags,
+            format="markdown",
+            caption=
+            f"# {title}\n\n## {v['description']}\n\n### [{price}]({k}) ({platforms_title})",
+            source=f"{v['gif'] if v['gif'] else v['image']}")
+
+        print(f"New game added: {k}")
+        DONE.append(k)
+        with open(DONE_FILE, "w") as f:
+            json.dump(DONE, f)
+
+    print(f"\nAll done! ({round(time.time()-DELTA)}s)")
+    input()
